978fd7d3e259fe1a7c68ad23cab83ddce0865c0d
Jeroen van Vliet <j.van.vliet@hotmail.com>
2020-01-28 10:41:14 +0100

    added changes to feedback.py

--- feedback.py
+++ feedback.py
@@ -1,39 +1,40 @@
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 import events
 
-class Feedback:
-    def __start_motor(self, interval):
+def start_motor(interval):
+    GPIO.output(OUTPUT_PINS["LED_WHITE_FRONT"],GPIO.HIGH)
+    sleep(interval)
+    GPIO.output(OUTPUT_PINS["LED_WHITE_FRONT"],GPIO.LOW)
+    sleep(interval)
+    GPIO.output(OUTPUT_PINS["LED_WHITE_FRONT"],GPIO.HIGH)
+
+
+def hit_wall(interval):
+    while True:
+        GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.HIGH)
+        sleep(interval)
+        GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.LOW)      
+        sleep(interval/2)
+
+def away_from_wall():
+    GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.LOW)
+
+
+def car_backward():
+    GPIO.output(OUTPUT_PINS["LED_RED_BACK"], GPIO.HIGH)
+
+def __car_forward():
+    GPIO.output(OUTPUT_PINS["LED_RED_BACK"], GPIO.LOW)
+
+def __balloon_popped(interval):
+    while True:
         GPIO.output(OUTPUT_PINS["LED_WHITE_FRONT"],GPIO.HIGH)
+        GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.HIGH)
         sleep(interval)
         GPIO.output(OUTPUT_PINS["LED_WHITE_FRONT"],GPIO.LOW)
-        sleep(interval)
-        GPIO.output(OUTPUT_PINS["LED_WHITE_FRONT"],GPIO.HIGH)
-
-    def __hit_wall(self, interval):
-        while True:
-            GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.HIGH)
-            sleep(interval)
-            GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.LOW)
-            sleep(interval/2)
-
-    def __away_from_wall(self):
         GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.LOW)
-
-    def __car_backward(self):
-        GPIO.output(OUTPUT_PINS["LED_RED_BACK"], GPIO.HIGH)
-
-    def __car_forward(self):
-        GPIO.output(OUTPUT_PINS["LED_RED_BACK"], GPIO.LOW)
-
-    def __balloon_popped(self, interval):
-        while True:
-            GPIO.output(OUTPUT_PINS["LED_WHITE_FRONT"],GPIO.HIGH)
-            GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.HIGH)
-            sleep(interval)
-            GPIO.output(OUTPUT_PINS["LED_WHITE_FRONT"],GPIO.LOW)
-            GPIO.output(OUTPUT_PINS["LED_RED_BACK"],GPIO.LOW)
-            sleep(interval)
+        sleep(interval)
 
 # Initialize local constants
 OUTPUT_PINS = {
@@ -44,3 +45,5 @@ OUTPUT_PINS = {
 # Initialize GPIO pins
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21,GPIO.OUT)
+
+start_motor(0.1)
