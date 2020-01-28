43ca5c830f049b9b562279983bd677fee73105a6
Floris272 <60342568+Floris272@users.noreply.github.com>
2020-01-28 13:14:30 +0100

    Update blynktest.py

--- blynktest.py
+++ blynktest.py
@@ -1,18 +1,33 @@
 import blynklib
 
 
-
 BLYNK_AUTH = 'mz2Zl4gY8cWqcg2Ag2latyNb7SylKGmG'
 
 # initialize Blynk
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
 
-# register handler for virtual pin V4 write event
-@blynk.handle_event('write V4')
-def read_virtual_pin_handler(pin, value):
-    if int(value[0]) == 1:
-        print('ja')
- 
+#links
+@blynk.handle_event('write V0')
+def links_input(pin, value):
+	if int(value[0]):
+		#NAAR LINKS
+		print(value, pin)
+
+
+#rechts
+@blynk.handle_event('write V1')
+def rechts_input(pin, value):
+	if int(value[0]):
+		#NAAR RECHTS
+		print(value, pin)
+
+#180 graden
+@blynk.handle_event('write V2')
+def rotate_input(pin, value):
+	if int(value[0]):
+		#180 GRADEN
+		print(value, pin)
+
 while True:
 	blynk.run()
